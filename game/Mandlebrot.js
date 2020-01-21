bd100843d548fb0c6ff4f7d49b0c750428f78eaf
Number97 <47658228+Number97@users.noreply.github.com>
2020-01-21 15:12:27 +0000

    Add files via upload
    adfg

--- game/Mandlebrot.js
+++ game/Mandlebrot.js
@@ -9,6 +9,10 @@ let stage
 let i,j
 let counter1 = 1
 let counter2 = 0
+let bar
+let counter3
+let trueDensity
+let radius
 
 class Point
 {
@@ -36,7 +40,7 @@ function setup()
 	xmin = -2
 	size = 4
 	ymin = -2
-	density = 4
+	density = 3
 	l = 0
 	mandlebrot = []
 	centerY=0
@@ -46,6 +50,9 @@ function setup()
 	j=ymin
 	counter1 = 1
 	counter2 = 0
+	counter3 = 1
+	bar = 7
+	translate(width/2,height/2)
 }
 
 function draw()
@@ -69,14 +76,98 @@ function draw()
 			line(-490,-450,-390,-450)
 			line(-490,-440,-390,-440)
 		
+			fill(0)
+			rect(190,-490,290,30)
+			switch(density)
+			{
+				case 1:
+					trueDensity=100
+					radius=1
+					break;
+				case 2:
+					trueDensity=90
+					radius=1
+					break;
+				case 3:
+					trueDensity=80
+					radius=2
+					break;
+				case 4:
+					trueDensity=70
+					radius=2
+					break;
+				case 5:
+					trueDensity=60
+					radius=3
+					break;
+				case 6:
+					trueDensity=50
+					radius=3
+					break;
+				case 7:
+					trueDensity=40
+					radius=4
+					break;
+				case 8:
+					trueDensity=30
+					radius=4
+					break;
+				case 9:
+					trueDensity=20
+					radius=5
+					break;
+				case 10:
+					trueDensity=10
+					radius=5
+					break;
+				case 20:
+					trueDensity=9
+					radius=10
+					break;
+				case 30:
+					trueDensity=8
+					radius=15
+					break;
+				case 40:
+					trueDensity=7
+					radius=20
+					break;
+				case 50:
+					trueDensity=6
+					radius=25
+					break;
+				case 60:
+					trueDensity=5
+					radius=30
+					break;
+				case 70:
+					trueDensity=4
+					radius=35
+					break;
+				case 80:
+					trueDensity=3
+					radius=40
+					break;
+				case 90:
+					trueDensity=2
+					radius=45
+					break;
+				case 100:
+					trueDensity=1
+					radius=50
+					break;
+			}
+			fill(255)
+			text(trueDensity,455,-470)
+			text("Density (change with UP AND DOWN arrows): ",200,-470)
 			
 			stage=0
 			break;
 		case 0:
 			line(-490+map((j-xmin)*((density*size)/2000)+i-xmin,0,(size)*((density*size)/2000)+size,0,100),-490,-490+map((j-xmin)*((density*size)/2000)+i-xmin,0,(size)*((density*size)/2000)+size,0,100),-480)
-			if((i!=0)&&(j!=0))
+			for(i=xmin;i<xmin+size;i+=(density*size)/2000)
 			{
-				if(j<ymin+size)
+				for(j=ymin;j<ymin+size;j+=(density*size)/2000)
 				{
 					x=i
 					y=j
@@ -98,33 +189,18 @@ function draw()
 						mandlebrot.push(new Point(x,y,0))
 						l++
 					}
-					j+=(density*size)/2000
-				}
-				else
-				{
-					if(i<xmin+size)
-					{
-						i+=(density*size)/2000
-						j=ymin
-					}
-					else
-					{
-						stage=1
-					}
 				}
 			}
-			else
-			{
-				i=xmin
-				j=ymin
-			}
+			stage=1
+			fill(0)
+			rect(-490,-490,100,10)
 			break;
 		
-
 		case 1:
 			if(counter1<13)
 			{
-				line(-490+map(counter1,1,12,0,100),-470,-490+map(counter1,1,12,0,100),-460)
+				fill(0)
+				rect(-490+map(counter1,1,13,0,100),-470,100/12,10)
 				changing((counter1-1)*30+1);
 				counter1++
 			}
@@ -135,7 +211,7 @@ function draw()
 			break;
 
 		case 2:
-			if(counter2<l)
+			while(counter2<counter3*l/bar)
 			{
 				stroke(0)
 				line(-490+map(counter2,0,l,0,100),-450,-490+map(counter2,0,l,0,100),-440)
@@ -188,15 +264,204 @@ function draw()
 					fill(0,191,17);
 				}
 				noStroke()
-				ellipse((mandlebrot[counter2].x-centerX)*(width/size),(mandlebrot[counter2].y-centerY)*(height/size),10,10);
+				ellipse((mandlebrot[counter2].x-centerX)*(width/size),(mandlebrot[counter2].y-centerY)*(height/size),radius,radius);
 				counter2++
+			}
+			if(counter3!=bar)
+			{
+				counter3++
 			}
 			else
 			{
 				noLoop()
 			}
+			
+			break;
+		case 5:
+			fill(0)
+			rect(190,-490,290,30)
+			switch(density)
+			{
+				case 1:
+					trueDensity=100
+					radius=1
+					break;
+				case 2:
+					trueDensity=90
+					radius=1
+					break;
+				case 3:
+					trueDensity=80
+					radius=2
+					break;
+				case 4:
+					trueDensity=70
+					radius=2
+					break;
+				case 5:
+					trueDensity=60
+					radius=3
+					break;
+				case 6:
+					trueDensity=50
+					radius=3
+					break;
+				case 7:
+					trueDensity=40
+					radius=4
+					break;
+				case 8:
+					trueDensity=30
+					radius=4
+					break;
+				case 9:
+					trueDensity=20
+					radius=5
+					break;
+				case 10:
+					trueDensity=10
+					radius=5
+					break;
+				case 20:
+					trueDensity=9
+					radius=10
+					break;
+				case 30:
+					trueDensity=8
+					radius=15
+					break;
+				case 40:
+					trueDensity=7
+					radius=20
+					break;
+				case 50:
+					trueDensity=6
+					radius=25
+					break;
+				case 60:
+					trueDensity=5
+					radius=30
+					break;
+				case 70:
+					trueDensity=4
+					radius=35
+					break;
+				case 80:
+					trueDensity=3
+					radius=40
+					break;
+				case 90:
+					trueDensity=2
+					radius=45
+					break;
+				case 100:
+					trueDensity=1
+					radius=50
+					break;
+			}
+			fill(255)
+			text(trueDensity,455,-470)
+			text("Density (change with UP AND DOWN arrows): ",200,-470)
 			break;
 	}
+	line(-490,-490,-490,-480)
+			line(110-500,-490,110-500,-480)
+			line(-490,-470,-490,-460)
+			line(110-500,-470,110-500,-460)
+			line(-490,-450,-490,-440)
+			line(110-500,-450,110-500,-440)
+
+			line(-490,-490,-390,-490)
+			line(-490,-480,-390,-480)
+			line(-490,-470,-390,-470)
+			line(-490,-460,-390,-460)
+			line(-490,-450,-390,-450)
+			line(-490,-440,-390,-440)
+		
+			fill(0)
+			rect(190,-490,290,30)
+			switch(density)
+			{
+				case 1:
+					trueDensity=100
+					radius=1
+					break;
+				case 2:
+					trueDensity=90
+					radius=1
+					break;
+				case 3:
+					trueDensity=80
+					radius=2
+					break;
+				case 4:
+					trueDensity=70
+					radius=2
+					break;
+				case 5:
+					trueDensity=60
+					radius=3
+					break;
+				case 6:
+					trueDensity=50
+					radius=3
+					break;
+				case 7:
+					trueDensity=40
+					radius=4
+					break;
+				case 8:
+					trueDensity=30
+					radius=4
+					break;
+				case 9:
+					trueDensity=20
+					radius=5
+					break;
+				case 10:
+					trueDensity=10
+					radius=5
+					break;
+				case 20:
+					trueDensity=9
+					radius=10
+					break;
+				case 30:
+					trueDensity=8
+					radius=15
+					break;
+				case 40:
+					trueDensity=7
+					radius=20
+					break;
+				case 50:
+					trueDensity=6
+					radius=25
+					break;
+				case 60:
+					trueDensity=5
+					radius=30
+					break;
+				case 70:
+					trueDensity=4
+					radius=35
+					break;
+				case 80:
+					trueDensity=3
+					radius=40
+					break;
+				case 90:
+					trueDensity=2
+					radius=45
+					break;
+				case 100:
+					trueDensity=1
+					radius=50
+					break;
+			}
+			fill(255)
+			text(trueDensity,455,-470)
+			text("Density (change with UP AND DOWN arrows): ",200,-470)
 }
 
 function modul(x,y)
@@ -230,8 +495,10 @@ function changing(iterations){
 
 function mousePressed()
 {
+	translate(width/2,height/2)
 	let x=mouseX-500;
 	let y=mouseY-500;
+	bar = 7
 	
 	xmin=x*size/width+centerX-(size*0.4)/2;
 	ymin=y*size/height+centerY-(size*0.4)/2;
@@ -242,33 +509,42 @@ function mousePressed()
 	background(0,191,17)
 	centerX=xmin+size/2
 	centerY=ymin+size/2
-	line(-490,-490,-490,-480)
-	line(110-500,-490,110-500,-480)
-	line(-490,-470,-490,-460)
-	line(110-500,-470,110-500,-460)
-	line(-490,-450,-490,-440)
-	line(110-500,-450,110-500,-440)
-
-	line(-490,-490,-390,-490)
-	line(-490,-480,-390,-480)
-	line(-490,-470,-390,-470)
-	line(-490,-460,-390,-460)
-	line(-490,-450,-390,-450)
-	line(-490,-440,-390,-440)
 
 	stage=-1
 	i=xmin
 	j=ymin
 	counter1=1
 	counter2=0
+	counter3=1
 	loop();
 }
 function keyPressed()
 {
-	if(key=='r'){
-		xmin=-2;
-		size=4;
-		ymin=-2;
-		redraw();
+	if(keyCode == UP_ARROW)
+	{
+		if((density<=10)&&(density!=1))
+		{
+			density--
+		}
+		else if(density>10)
+		{
+			density-=10
+		}
+		stage=5
+		redraw()
+	}
+
+	if(keyCode == DOWN_ARROW)
+	{
+		if(density<10)
+		{
+			density++
+		}
+		else if((density>=10)&&(density<100))
+		{
+			density+=10
+		}
+		stage=5
+		redraw()
 	}
 }
